cb2f8bb6b52de1d7c43623a6823b3f44f1d11284
Carlos Serrano <CSerrano@lbl.gov>
2018-02-02 17:17:10 -0800

    Modified documentation/conf.py so it works for me

--- documentation/conf.py
+++ documentation/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('../src/python'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+#    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
