b4d8c625d838a17e068f9cbc211d2cd4da524437
Carlos Serrano <CSerrano@lbl.gov>
2018-08-02 16:29:29 -0700

    make get_channel_mask read and not write

--- src/python/leep/raw.py
+++ src/python/leep/raw.py
@@ -155,7 +155,7 @@ class LEEPDevice(DeviceBase):
         self.reg_write([('chan_keep', chans)], instance=instance)
 
     def get_channel_mask(self, instance=[]):
-        chans, =  self.reg_write(['chan_keep'], instance=instance)
+        chans, =  self.reg_read(['chan_keep'], instance=instance)
         return chans
 
     def wait_for_acq(self, tag=False, toggle_tag=False, timeout=5.0, instance=[]):
