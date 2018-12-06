osprey-dcs/feed-core

commit 058f6a9517891f6c2cdd976e81b541481cbd2a77

Fix unsigned handling on CA backend

src/python/leep/ca.py
@@ -93,6 +93,9 @@ class CADevice(DeviceBase):
                 name = self.expand_regname(name, instance=instance)
             info = self._info[name]
             pvname = str(info['output'])
+            if info.get('sign', 'unsigned') == 'unsigned' and (value & 0x80000000):
+                # CA only has signed integers
+                value = -((value ^ 0xffffffff)+1)
             caput(pvname, value, wait=True, timeout=self.timeout)
 
     def reg_read(self, names, instance=[]):

commit 5c3cc7199057b94655314292ee767313bd1359ad

Fixed ca.py
Logic added in commit 058f6a95 broke reg_write method for arrays.

src/python/leep/ca.py
@@ -93,9 +93,9 @@ class CADevice(DeviceBase):
                 name = self.expand_regname(name, instance=instance)
             info = self._info[name]
             pvname = str(info['output'])
-            if info.get('sign', 'unsigned') == 'unsigned' and (value & 0x80000000):
-                # CA only has signed integers
-                value = -((value ^ 0xffffffff)+1)
+            # if info.get('sign', 'unsigned') == 'unsigned' and (value & 0x80000000):
+            # CA only has signed integers
+            # value = -((value ^ 0xffffffff)+1)
             caput(pvname, value, wait=True, timeout=self.timeout)
 
     def reg_read(self, names, instance=[]):