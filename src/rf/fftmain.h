f07d28270ba1534669e5c39b181917b5d66bad77
Sonya Hoobler <sonya@slac.stanford.edu>
2021-02-16 14:07:12 -0800

    Revert "Revert "RF waveform FFTs: add support for real-to-complex transforms, and in particular detune frequency""
    This reverts commit d55c022174136373663512a05a3eff6a1855e8b3.

--- src/rf/fftmain.h
+++ src/rf/fftmain.h
@@ -9,6 +9,12 @@
 #define FFT_MAX_SIG 10 /* Maximum waveform signals per channel (per cavity) 
 						* these will be processed by single thread
 						*/
+#define FFT_MAX_PLAN 2
+
+/* Supported FFT conversion types */
+#define FFT_TYPE_C2C 0 /* Complex to complex, fftwf_plan_dft_1d */
+#define FFT_TYPE_R2C 1 /* Realize to complex, fftwf_plan_dft_r2c_1d*/
+
 
 epicsMutexId  fftInitTaskMutex; /* Global mutex for use when initializing FFT tasks */
 
@@ -17,18 +23,22 @@ typedef struct FFTDataRec_ {
 	char          thread_name[MAX_NAME_LENGTH];
 	epicsMutexId  mutex;
 	size_t        fft_max_len;	        /* Maximum input/output data length for any signal */
-	size_t        len[FFT_MAX_SIG];     /* Per-signal length of last data set */
+	size_t        len[FFT_MAX_SIG];     /* Per-signal input length of last data set */
+	size_t        len_output[FFT_MAX_SIG];     /* Per-signal output length of last data set */
 	double        tstep[FFT_MAX_SIG];   /* Per-signal time step of last data set */
-	double       *data;	                /* FFT results, shared between FFT thread and device support */
+	double        fstep[FFT_MAX_SIG];   /* Per-signal frequency step of last data set */
+	double       *data;	                /* FFT results, I and Q output, shared between FFT thread and device support */
+	double       *freq;	                /* Frequency array for FFT output, shared between FFT thread and device support */
 	epicsMessageQueueId queue_id;
 } FFTDataRec, *FFTData;
 
 typedef struct FFTMsgRec_ {
-	size_t   len;    /* Data number of elements */
+	size_t   len;    /* Data input number of elements */
 	double   tstep;  /* Data time step (spacing between data points) */
+	int      type;   /* FFT type, see FFT_TYPE_* above */
 	int      index;  /* Index of this signal, used as index into FFTData data array */
-	double   *in_re;
-	double   *in_im;
+	double   *in_re; /* Input time-domain real part */
+	double   *in_im; /* Input time-domain imaginary part */
 	EVENTPVT event;  /* Per-signal database scan event */
 	int      debug;
 } FFTMsgRec, *FFTMsg;
@@ -37,7 +47,7 @@ FFTData fftDataFind(const char *name);
 
 int rfFFTTaskInit(char *name, size_t fft_max_len);
 
-int fftMsgPost(epicsMessageQueueId queue_id, size_t len, double *in_re, double *in_im, double tstep, int index, EVENTPVT event, int debug);
+int fftMsgPost(epicsMessageQueueId queue_id, size_t len, double *in_re, double *in_im, double tstep, int index, EVENTPVT event, int debug, int type);
 
 
 #endif /* FFT_H */
